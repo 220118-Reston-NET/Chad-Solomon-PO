40b3f3bad0d73cbfb1b2b029fc40242cdaa1b143
Chad Solomon <chadcsoloman@gmail.com>
2022-01-27 11:17:33 -0500

    firts commit

--- PokeUI/Program.cs
+++ PokeUI/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PokeBL;
+using PokeDL;
 using PokeModel;
 using PokeUI;
+
 
 bool repeat = true;
 IMenu menu = new MainMenu();
@@ -12,6 +15,22 @@ IMenu menu = new MainMenu();
 >the store could sell all of the potato parts. 
 >
 
+
+
+***** Idea for the for the products Menus:
+> Once a customer picks a product, we could try to suggest another product that would 
+    go well with their origonal purchase.
+    for instance....if a customer buys a leash we could suggest a new collar also!
+                    or if they buy a toy, we could suggest some treats.
+
+******* Idea for for functionality
+
+> Once a customer picks a product form the category they chose I should ask what quantity they would like and show the price and description fo the item.
+
+
+dotnet add package Serilog.Sinks.File
+dotnet add package
+dotnet add package Serilog
 
 */
 
@@ -30,7 +49,24 @@ while (repeat)
             break;
 
         case "CustomerAccount":
-            menu = new CustomerAccount();
+            menu = new CustomerAccount(new CustomerBL(new Repository()));
+            break;
+
+
+        case "LeashMenu":
+            menu = new LeashMenu();
+            break;
+
+        case "CollarMenu":
+            menu = new CollarMenu();
+            break;
+
+        case "ToyMenu":
+            menu = new ToyMenu();
+            break;
+
+        case "TreatMenu":
+            menu = new TreatMenu();
             break;
 
         default:
