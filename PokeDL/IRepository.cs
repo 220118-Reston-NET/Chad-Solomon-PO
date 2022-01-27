4e6409926e84c5207b6580619fb00e9940184765
Chad Solomon <chadcsoloman@gmail.com>
2022-01-27 11:15:51 -0500

    first commit

---
+++ PokeDL/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using PokeModel;
+using System.Text.Json;
+namespace PokeDL
+{
+
+    /*
+
+
+    */
+    public interface IRepository
+    {
+        Customer AddCustomer(Customer c_name);
+
+
+        // public List<Customer> GetAllCustomers()
+        // {
+
+        // }
+
+
+
+    }
+}
