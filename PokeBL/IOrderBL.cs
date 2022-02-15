b0dad9e1045d12b5f85d9fc7d0587fadb7dddd6e
Chad Solomon <chadcsoloman@gmail.com>
2022-02-15 01:35:07 -0500

    latest commit

---
+++ PokeBL/IOrderBL.cs
@@ -0,0 +1,18 @@
+using PokeModel;
+namespace PokeBL
+{
+    public interface IOrderBL
+    {
+
+        public void AddOrder(int _orderLocation, int _price, int _custID);
+
+        // public List<Order> SearchOrder(string c_name);
+    }
+
+
+
+
+
+
+
+}
