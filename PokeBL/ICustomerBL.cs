312ac290e6339d897a855700439185dbae0cdd05
Chad Solomon <chadcsoloman@gmail.com>
2022-02-15 01:34:11 -0500

    Latest commit

---
+++ PokeBL/ICustomerBL.cs
@@ -0,0 +1,12 @@
+﻿using PokeModel;
+
+namespace PokeBL
+{
+    public interface IPokemonBL
+    {
+        public Customer AddCustomer(Customer c_name);
+
+        public List<Customer> SearchCustomer(int c_id);
+    }
+
+}
