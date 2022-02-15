428145497bcdb6a22ae98ad557bc4b83d651cc88
Chad Solomon <chadcsoloman@gmail.com>
2022-02-15 11:41:34 -0500

    latest commit

---
+++ PokeModel/StoreOrder.cs
@@ -0,0 +1,94 @@
+namespace PokeModel
+{
+
+
+    public class StoreOrder
+    {
+
+        public int _orderID;
+        public string? _custName;
+
+        public string? _storeName;
+
+        // private string _orderName;
+        // public string Name
+        // {
+
+        //     get { return _orderName; }
+
+        //     set
+        //     {
+        //         if (value != null)
+        //         {
+        //             _orderName = value;
+
+        //         }
+        //     }
+        // }
+        private string _lineItems;
+        public string LineItems
+        {
+
+            get { return _lineItems; }
+
+            set
+            {
+                if (value != null)
+                {
+                    _lineItems = value;
+
+                }
+            }
+        }
+
+        private string _storeFrontLocation;
+        public string StoreLocation
+        {
+            get { return _storeFrontLocation; }
+            set
+            {
+                _storeFrontLocation = value;
+            }
+
+        }
+
+        public int _storeID;
+        private decimal _totalPrice;
+        public decimal TotalPrice
+        {
+            get { return _totalPrice; }
+            set
+            {
+                if (value > 0)
+                {
+                    _totalPrice = value;
+                }
+            }
+        }
+
+
+        // public Order()
+        // {
+        //     _orderID = 0;
+        //     _lineItems = new string()
+        //     {
+        //         new LineItems()
+        //     };
+        //     StoreLocation = "None";
+        //     TotalPrice = 0;
+
+
+
+        // }
+
+
+        public override string ToString()
+        {
+            return $"Store Name: {_storeName}\nStore ID: {_storeID}\nStore Location: {StoreLocation}\nOrder ID: {_orderID}\n";
+        }
+
+
+
+
+    }
+}
