d41dd10f883149b0e7b1c5c5dfab6a022d1853ff
Chad Solomon <chadcsoloman@gmail.com>
2022-02-15 11:36:44 -0500

    latest commit

---
+++ PokeBL/ProductsBL.cs
@@ -0,0 +1,39 @@
+using PokeDL;
+using PokeModel;
+namespace PokeBL
+{
+
+    public class ProductBL : IProductBL
+    {
+
+        //Dependency Injection
+
+        private IProductRepo _orepo;
+        public ProductBL(IProductRepo o_repo)
+        {
+            _orepo = o_repo;
+        }
+
+        public Product AddProduct(Product c_product)
+        {
+            return _orepo.AddProduct(c_product);
+
+        }
+
+        public List<Product> SearchProduct(string c_product)
+        {
+            List<Product> listOfProduct = _orepo.GetAllProduct();
+
+            //LINQ Library
+            //.Where() filters a sequence of values based on a predicate
+            //.ToList used with .Where or other methods from the IEnumerable class to place the out from the IEnumerable into a list.
+            return listOfProduct
+                    .Where(prod => prod.Name.Contains(c_product))
+                    .ToList();
+
+
+        }
+
+
+    }
+}
