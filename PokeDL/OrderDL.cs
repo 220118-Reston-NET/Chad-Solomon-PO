847186cff2b8fb4b6c294265034a561c9e796e29
Chad Solomon <chadcsoloman@gmail.com>
2022-02-15 11:38:41 -0500

    latest commit

---
+++ PokeDL/OrderDL.cs
@@ -0,0 +1,36 @@
+// using PokeModel;
+// namespace PokeDL
+// {
+//     public class OrderDL : IOrderRepo
+//     {
+
+//         // public void AddOrder(int _orderLocation, int _price, int _custID)
+//         // {
+//         //     //Setting listOfOrder = to GetALlOrder(); since it returns a list of orders.
+//         //     List<Order> listOfOrder = GetOrder();
+
+//         //     //Adding the specific order to the listOfOrder's
+//         //     listOfOrder.Add(_orderLocation);
+//         //     listOfOrder.Add(_price);
+//         //     listOfOrder.Add(_custID);
+
+//         //     //return c_order;
+//         // }
+
+
+//         public List<int> GetOrder()
+//         {
+//             List<int> listOfOrder = new List<int>();
+
+//             return listOfOrder;
+//         }
+
+//         public List<Order> GetAllOrder()
+//         {
+
+//             List<Order> listOfOrders = new List<Order>();
+
+//             return listOfOrders;
+//         }
+//     }
+// }
